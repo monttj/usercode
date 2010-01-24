19d831c6e32a7018f9415c0a7dc4d2e387630370
tjkim <>
2010-01-24 22:33:35 +0000

    update vector type histogram

--- PFA/PFAnalyses/PFCandidate/test/Macro/isoCompare.C
+++ PFA/PFAnalyses/PFCandidate/test/Macro/isoCompare.C
@@ -1,69 +1,108 @@
+#include <vector>
+using namespace std;
+
 void isoCompare(){
 
-  Chain * tree_data = new Chain("Summary/tree", "../../PFAnalysis_RD_VeryClean.root");
-  Chain * tree_mc = new Chain("Summary/tree", "../../PFAnalysis_MC_Dec6.root");
+  Chain * tree_data = new Chain("Summary/tree", "../../PFAnalysis_RD_Jan22.root");
+  Chain * tree_mc = new Chain("Summary/tree", "../../PFAnalysis_MC_Jan22.root");
+  //Chain * tree_mc = new Chain("Summary/tree", "../../PFAnalysis_RD_VeryClean_v341.root");
 
   TCut cutEndcap = "(ChargedHadrons.Eta() > 1.0 || ChargedHadrons.Eta() < -1.0)";
   TCut cutBarrel = "(ChargedHadrons.Eta() < 1.0 && ChargedHadrons.Eta() > -1.0)";
   TCut cutHcal = "ChargedHadrons.hcal() > 0"; 
   TCut cutnhit = "ChargedHadrons.nHits() >= 3";
-  TCut cut= cutnhit;
+  TCut cutpT = "ChargedHadrons.Pt() > 1";
+  TCut cut = cutnhit;
 
   vector<string> hName;
-  hName.push_back("Charged");
-  hName.push_back("Neutral");
-  hName.push_back("Gamma");
+  hName.push_back("Charged deposit");
+  hName.push_back("Neutral deposit");
+  hName.push_back("Gamma deposit");
+  hName.push_back("Charged #Delta R");
+  hName.push_back("Neutral #Delta R");
+  hName.push_back("Gamma #Delta R");
   hName.push_back("Multiplicity");
   hName.push_back("pT");
   hName.push_back("eta");
+  hName.push_back("track_Multiplicity");
+  hName.push_back("track_pT");
+  hName.push_back("track_eta");
+
 
   vector<string> var;
-  var.push_back("ChargedHadrons.isoDeposit(4).depositWithin(0.5)");
-  var.push_back("ChargedHadrons.isoDeposit(5).depositWithin(0.5)");
-  var.push_back("ChargedHadrons.isoDeposit(6).depositWithin(0.5)");
+  var.push_back("ChargedHadrons.isoDeposit(4).depositWithin(0.3)");
+  var.push_back("ChargedHadrons.isoDeposit(5).depositWithin(0.3)");
+  var.push_back("ChargedHadrons.isoDeposit(6).depositWithin(0.3)");
+  var.push_back("ChargedHadrons.isoDeposit(4).first.deltaR");
+  var.push_back("ChargedHadrons.isoDeposit(5).first.deltaR");
+  var.push_back("ChargedHadrons.isoDeposit(6).first.deltaR");
   var.push_back("@ChargedHadrons.size()");
   var.push_back("ChargedHadrons.Pt()");
   var.push_back("ChargedHadrons.Eta()");
-  
+  var.push_back("@Tracks.size()");
+  var.push_back("Tracks.Pt()");
+  var.push_back("Tracks.Eta()");
+ 
+ 
   vector<string> title;
-  title.push_back("Energy deposit within cone (GeV)");
-  title.push_back("Energy deposit within cone (GeV)");
-  title.push_back("Energy deposit within cone (GeV)");
+  title.push_back("Charged Hadron #Sigma E within cone (GeV)");
+  title.push_back("Neutral Hadron #Sigma E within cone (GeV)");
+  title.push_back("Photon #Sigma E within cone (GeV)");
+  title.push_back("Charged Hadron #Delta R");
+  title.push_back("Neutral Hadron #Delta R");
+  title.push_back("Photon #Delta R");
   title.push_back("Multiplicity");
   title.push_back("Transverse momentum (GeV)");
   title.push_back("Pseudorapidity #eta");
+  title.push_back("track_Multiplicity");
+  title.push_back("track_Transverse momentum (GeV)");
+  title.push_back("track_Pseudorapidity #eta");
+ 
 
-  TH1* h_data[6];
-  TH1* h_mc[6];
+  vector<TH1*> h_data;
+  vector<TH1*> h_mc;
+  //Data
+  h_data.push_back(new TH1D(Form("h_data_%s",hName[0]),"h_data",80,0,4));
+  h_data.push_back(new TH1D(Form("h_data_%s",hName[1]),"h_data",80,0,4));
+  h_data.push_back(new TH1D(Form("h_data_%s",hName[2]),"h_data",80,0,4));
+  h_data.push_back(new TH1D(Form("h_data_%s",hName[3]),"h_data",100,0,1));
+  h_data.push_back(new TH1D(Form("h_data_%s",hName[4]),"h_data",100,0,1));
+  h_data.push_back(new TH1D(Form("h_data_%s",hName[5]),"h_data",100,0,1));
+  h_data.push_back(new TH1D(Form("h_data_%s",hName[6]),"h_data",80,0,80));
+  h_data.push_back(new TH1D(Form("h_data_%s",hName[7]),"h_data",100,0,10));
+  h_data.push_back(new TH1D(Form("h_data_%s",hName[8]),"h_data",30,-3,3));
+  h_data.push_back(new TH1D(Form("h_data_%s",hName[9]),"h_data",80,0,80));
+  h_data.push_back(new TH1D(Form("h_data_%s",hName[10]),"h_data",100,0,10));
+  h_data.push_back(new TH1D(Form("h_data_%s",hName[11]),"h_data",30,-3,3));
 
-  for(int i=0; i< 3 ; i++){
-      h_data[i]= new TH1D(Form("h_data_%s",hName[i]),"h_data",80,0,4);
-      h_mc[i] =new TH1D(Form("h_mc_%s",hName[i]),"h_mc",80,0,4);
-  }
-  h_data[3]= new TH1D(Form("h_data_%s",hName[3]),"h_data",80,0,80);
-  h_mc[3] =new TH1D(Form("h_mc_%s",hName[3]),"h_mc",80,0,80);
-  h_data[4]= new TH1D(Form("h_data_%s",hName[4]),"h_data",100,0,10);
-  h_mc[4] =new TH1D(Form("h_mc_%s",hName[4]),"h_mc",100,0,10);
-  h_data[5]= new TH1D(Form("h_data_%s",hName[5]),"h_data",30,-3,3);
-  h_mc[5] =new TH1D(Form("h_mc_%s",hName[5]),"h_mc",30,-3,3);
+  //MC 
+  h_mc.push_back(new TH1D(Form("h_mc_%s",hName[0]),"h_mc",80,0,4));
+  h_mc.push_back(new TH1D(Form("h_mc_%s",hName[1]),"h_mc",80,0,4));
+  h_mc.push_back(new TH1D(Form("h_mc_%s",hName[2]),"h_mc",80,0,4));
+  h_mc.push_back(new TH1D(Form("h_mc_%s",hName[3]),"h_mc",100,0,1));
+  h_mc.push_back(new TH1D(Form("h_mc_%s",hName[4]),"h_mc",100,0,1));
+  h_mc.push_back(new TH1D(Form("h_mc_%s",hName[5]),"h_mc",100,0,1));
+  h_mc.push_back(new TH1D(Form("h_mc_%s",hName[6]),"h_mc",80,0,80));
+  h_mc.push_back(new TH1D(Form("h_mc_%s",hName[7]),"h_mc",100,0,10));
+  h_mc.push_back(new TH1D(Form("h_mc_%s",hName[8]),"h_mc",30,-3,3));
+  h_mc.push_back(new TH1D(Form("h_mc_%s",hName[9]),"h_mc",80,0,80));
+  h_mc.push_back(new TH1D(Form("h_mc_%s",hName[10]),"h_mc",100,0,10));
+  h_mc.push_back(new TH1D(Form("h_mc_%s",hName[11]),"h_mc",30,-3,3));
+
 
   double num_data = tree_data->GetEntries();
   double num_mc = tree_mc->GetEntries();
   double scale = num_data/num_mc;
 
-  plot(tree_data, tree_mc, h_data[0], h_mc[0], var[0], hName[0], title[0], cut, scale);
-  plot(tree_data, tree_mc, h_data[1], h_mc[1], var[1], hName[1], title[1], cut, scale);
-  plot(tree_data, tree_mc, h_data[2], h_mc[2], var[2], hName[2], title[2], cut, scale);
-  plot(tree_data, tree_mc, h_data[3], h_mc[3], var[3], hName[3], title[3], cut, scale);
-  plot(tree_data, tree_mc, h_data[4], h_mc[4], var[4], hName[4], title[4], cut, scale);
-  plot(tree_data, tree_mc, h_data[5], h_mc[5], var[5], hName[5], title[5], cut, scale);
-
+  for(int i=0; i < hName.size() ; i++){
+    plot(tree_data, tree_mc, h_data[i], h_mc[i], var[i], hName[i], title[i], cut, scale);
+  }
 }
 
 void plot(TChain *tree_data, TChain *tree_mc, TH1* h_data, TH1* h_mc, const TString &var, const TString& hName, const TString& title, TCut cut, const double& scale){
 
   TCanvas* c = new TCanvas(Form("c_%s",hName.Data()),Form("c_%s",hName.Data()),1); 
-  if(hName == "Charged" || hName == "Neutral" || hName == "Gamma" ) gPad->SetLogy();
+  if(hName == "Charged deposit" || hName == "Neutral deposit" || hName == "Gamma deposit" ) gPad->SetLogy();
 
   tree_data->Draw(Form("%s>>h_data_%s",var.Data(),hName.Data()),cut,"E");
   tree_mc->Draw(Form("%s>>h_mc_%s",var.Data(),hName.Data()),cut,"same");
@@ -71,6 +110,7 @@ void plot(TChain *tree_data, TChain *tree_mc, TH1* h_data, TH1* h_mc, const TStr
   h_data->SetMarkerStyle(kFullCircle);
   h_data->SetMarkerSize(0.5);
   h_data->SetStats(0);
+  h_data->SetTitle(0);
   h_data->GetXaxis()->SetTitle(title.Data());
   h_data->SetTitle(hName.Data());
   h_mc->SetLineColor(2);
