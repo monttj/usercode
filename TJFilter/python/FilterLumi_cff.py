6b174c248b5734d41adde8a946ab35e64590295e
tjkim <>
2010-04-30 11:19:48 +0000

    This commit was manufactured by cvs2svn to create tag 'B_test'.
    Sprout from master 2010-04-30 11:19:47 UTC tjkim 'modify for fixed direction for random cone'
    Delete:
        MinBias/test/Macro/compareTrig.C
        MinBias/test/Macro/compareTrigAsym.C
        MinBias/test/Macro/compareTrigVari.C
        PFA/PFAnalyses/W/scripts/setupPF.sh
        PFA/PFAnalyses/Z/BuildFile
        PFA/PFAnalyses/Z/bin/analyzeElectrons.cc
        PFA/PFAnalyses/Z/bin/analyzeMuons.cc
        PFA/PFAnalyses/Z/interface/PFElectronSelector.h
        PFA/PFAnalyses/Z/interface/PatMuonSelector.h
        PFA/PFAnalyses/Z/interface/TagProveZmmAnalyzer.h
        PFA/PFAnalyses/Z/interface/TestAnalyzer.h
        PFA/PFAnalyses/Z/interface/TestHistograms.h
        PFA/PFAnalyses/Z/src/PFElectronSelector.cc
        PFA/PFAnalyses/Z/src/PatMuonSelector.cc
        PFA/PFAnalyses/Z/src/TagProveZmmAnalyzer.cc
        PFA/PFAnalyses/Z/src/TestAnalyzer.cc
        PFA/PFAnalyses/Z/src/TestHistograms.cc
        PFA/PFAnalyses/Z/test/Demo_cfg.py
        PFA/PFAnalyses/Z/test/PatMuon_cfg.py
        RPC/Beamhalo/beamhaloplot.C
        RPC/Beamhalo/compbxplot.C
        RPC/Beamhalo/compplot.C
        RPC/Beamhalo/rmsplot.C
        TJFilter/BuildFile
        TJFilter/python/FilterBX_cff.py
        TJFilter/python/FilterL1Algo_cff.py
        TJFilter/python/FilterL1_cff.py
        TJFilter/python/FilterLumi_cff.py
        TJFilter/python/FilterRun_cff.py
        TJFilter/python/Filter_cff.py
        TJFilter/python/__init__.py
        TJFilter/src/FilterBX.cc
        TJFilter/src/FilterL1Algo.cc
        TJFilter/src/FilterLumi.cc
        TJFilter/src/FilterRun.cc
        TJFilter/src/TJFilter.cc
        TJFilter/test/crab.cfg
        TJFilter/test/crab.history
        TJFilter/test/numEve.sh
        TJFilter/test/server_caf.conf
        TJFilter/test/tjfilter_cfg.py
        TJFilter/test/tjfilter_cfg.pyc

--- TJFilter/python/FilterLumi_cff.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-###### Luminosity blocks by run when tracker and pixel HV were on ######
-goodLumiBlocks = cms.EDFilter("FilterLumi",
-    applyLumiFilter = cms.untracked.bool(True)
-)
-
